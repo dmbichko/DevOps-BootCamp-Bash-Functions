70e34b1f4105c473843c17d6f8ff3170bcb60aae
dmbichko <dmitrijbichko@gmail.com>
2022-07-10 19:38:55 +0300

    test function

--- functions.sh
+++ functions.sh
@@ -6,9 +6,16 @@ function pow () {
 function shortest() {
  for ARG in "$@"
  do
-     echo $ARG
+	lenght=${ARG}
+	 [ $i == 0 ] && min_lenght=$lenght 
+	if [[  $lenght -le $prev_lengt ]]; then
+		min_str=$ARG
+		min_lenght=$lenght
+	fi
+	 #echo $ARG
 
  done
+ echo $min_str ":" $minlenght
 }
 function print_log () {
         echo  "["$(date +"%Y-%m-%d %H:%M")"]" $1
