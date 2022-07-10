ae4cb237658b20202c90c8419606182cfec4c1db
Dzmitry Bichko <dzmitry_bichko@epam.com>
2022-07-10 20:02:15 +0300

    2part_check_string

--- functions.sh
+++ functions.sh
@@ -4,18 +4,30 @@ function pow () {
         #echo "This is pow"
 }
 function shortest() {
+i=0
+flag=0
  for ARG in "$@"
  do
-	lenght=${ARG}
-	 [ $i == 0 ] && min_lenght=$lenght 
-	if [[  $lenght -le $prev_lengt ]]; then
+	lenght=${#ARG}
+	if [ $i == 0 ]; then
+		min_lenght=$lenght
+		min_str=$ARG
+	fi
+	if [[  $lenght -le $min_lenght ]]; then
 		min_str=$ARG
 		min_lenght=$lenght
 	fi
 	 #echo $ARG
-
- done
- echo $min_str ":" $minlenght
+	let "i=i+1 "
+done
+for ARG in "$@"
+do
+	if [[ $min_lenght -eq ${#ARG} ]]; then
+		flag=1
+		echo $ARG
+	fi
+done
+	[ $flag == 0 ] && echo $min_str
 }
 function print_log () {
         echo  "["$(date +"%Y-%m-%d %H:%M")"]" $1
