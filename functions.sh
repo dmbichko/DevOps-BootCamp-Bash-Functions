748747888423331b2cecb6d1e6a59ffa0dc1cbf7
Dzmitry Bichko <dzmitry_bichko@epam.com>
2022-07-10 11:14:28 +0300

    pow and print log

--- functions.sh
+++ functions.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+function pow () {
+        echo $(($1**$2))
+        #echo "This is pow"
+}
+function shortest() {
+ for ARG in "$@"
+ do
+     echo $ARG
+
+ done
+}
+function print_log () {
+        echo  "["$(date +"%Y-%m-%d %H:%M")"]" $1
+}
 
 # Put your code here
